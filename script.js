61dd126fbc710fc625351777fd9ffc4035129368
SUBASRI-D <subasridurai07@gmail.com>
2024-08-11 00:26:37 +0530

    changed javascript

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-//variables
-const val=2;
-console.log(val)
-document.getElementsById("result").innerhtml="hello";
+let button=document.getElementById('Add');
+let todolist=document.getElementById('todolist');
+let input=document.getElementById('input');
+let todos=[];
 
+button.addEventListener('click',()=>{
+    todos.push(input.value)
+    addtodo(input.value)
+    input.value=''
+})
+function addtodo(todo){
+    let para = document.createElement('p');
+        para.innerText=todo;
+        todolist.appendChild(para);
+
+    
+}
