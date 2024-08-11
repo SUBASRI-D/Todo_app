e4b9ef9ab372ac2802968370f3baefbd4d81aaab
SUBASRI-D <subasridurai07@gmail.com>
2024-08-11 22:32:58 +0530

    changed the javascript code

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
-let inputField= document.getElementById("input");  //getting input value and store it in inputfield  using id name
-let button = document.getElementById("add");       //getting button  using id name
-let notesList=document.getElementById("notesList");//geetin ul by using id name
-function addTodo()  //function(it will execute when user click the button)
+let inputField= document.getElementById("input");  
+let button = document.getElementById("add");       
+let notesList=document.getElementById("notesList");
+function addTodo() 
 {
-    let inputValue=inputField.value.trim(); //remove the space from the user input
+    let inputValue=inputField.value.trim(); 
 
-    if(inputValue!==""){    //there is no space in the input bix if block is executed
-        let list = document.createElement('li');  //getting the element from the inputfiled and store it into list
+    if(inputValue!==""){   
+        let list = document.createElement('li');  
    
     
-        list.textContent=inputField.value.trim(); //remove the space
-        list.innerHTML=` ${inputValue}<button class="delete">Delete </button>`;//adding delete button
+        list.textContent=inputField.value.trim(); 
+        list.innerHTML=` ${inputValue}<button class="delete">Delete </button>`;
 
-        notesList.appendChild(list); // append the list value into ul list(notesList )
+        notesList.appendChild(list); 
     
       
-        inputField.value="";  //remove the value from the input box after printding it into list
+        inputField.value="";  
         
     }
 }
-document.addEventListener("click",(events)=>{  //function for delete button
-    if(events.target.classList.contains("delete")) //if the user click the button do the below operation
+document.addEventListener("click",(events)=>{  
+    if(events.target.classList.contains("delete")) 
     {
-        events.target.parentElement.remove();  //remove the added value
+        events.target.parentElement.remove();  
     }
 })
